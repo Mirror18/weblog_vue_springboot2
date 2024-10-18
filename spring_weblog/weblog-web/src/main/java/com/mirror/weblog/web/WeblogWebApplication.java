2b50172b5a4c18122cf090e7954089dd6592d4ed
mirror <mirrorcloud3@gmail.com>
2024-10-18 19:00:19 +0800

    创建多模块

---
+++ spring_weblog/weblog-web/src/main/java/com/mirror/weblog/web/WeblogWebApplication.java
@@ -0,0 +1,15 @@
+package com.mirror.weblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author mirror
+ */
+@SpringBootApplication
+public class WeblogWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeblogWebApplication.class,args);
+    }
+}
