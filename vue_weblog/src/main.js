00860e07661f2658efed104d3d53a8836b32d613
mirror <mirrorcloud3@gmail.com>
2024-10-21 02:18:58 +0800

    添加动画

--- vue_weblog/src/main.js
+++ vue_weblog/src/main.js
@@ -3,6 +3,7 @@ import App from '@/App.vue'     // 引入 App.vue 组件
 // 导入路由
 import router from '@/router'
 import './assets/main.css'      // 引入 main.css 样式文件
+import 'animate.css';
 // 导入 Element Plus 图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
