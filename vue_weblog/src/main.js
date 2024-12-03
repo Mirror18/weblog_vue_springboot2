9c41bc36976cac9cbe896096533f547d5ab30e2c
mirror <mirrorcloud3@gmail.com>
2024-12-03 23:47:16 +0800

    创建store，管理应用程序的状态。pinia

--- vue_weblog/src/main.js
+++ vue_weblog/src/main.js
@@ -13,6 +13,11 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 //导入 nprogress的css库
 import 'nprogress/nprogress.css'
 
+// 引入全局状态管理 Pinia
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
+
 // 创建应用，并将 App 根组件挂载到 <div id="#app"></div> 中
 const app = createApp(App)
 // 引入图标
@@ -22,3 +27,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 // 应用路由
 app.use(router)
 app.mount('#app')
+// 应用 Pinia
+app.use(pinia)
