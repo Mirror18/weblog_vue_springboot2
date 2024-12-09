de129ed8fd34a3ad74d8d83cca1a9b86fbd917cb
mirror <mirrorcloud3@gmail.com>
2024-12-09 19:59:39 +0800

    完成退出登录，修改用户名功能
    Signed-off-by: mirror <mirrorcloud3@gmail.com>

--- vue_weblog/src/composables/util.js
+++ vue_weblog/src/composables/util.js
@@ -1,8 +1,9 @@
-import {ElMessage} from "element-plus";
+import {ElMessage, ElMessageBox} from "element-plus";
 //手动引入组件的样式文件
 import 'element-plus/es/components/message/style/css'
 
 import nprogress from 'nprogress'
+import 'element-plus/es/components/message-box/style/css'
 
 export function showMessage(message = '提示内容',type='success',customClass =''){
 
@@ -21,4 +22,17 @@ export function showPageLoading(){
 //隐藏页面加载 loading
 export function hidePageLoading(){
     nprogress.done();
+}
+
+// 弹出确认框
+export function showModel(content = '提示内容', type = 'warning', title = '') {
+    return ElMessageBox.confirm(
+        content,
+        title,
+        {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type,
+        }
+    )
 }
