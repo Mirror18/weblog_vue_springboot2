b993d190267f9338b266d0194178f4e075121370
mirror <mirrorcloud3@gmail.com>
2024-12-09 16:20:18 +0800

    数据持久化插件 pinia-persist

--- vue_weblog/src/main.js
+++ vue_weblog/src/main.js
@@ -12,11 +12,10 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 //导入 nprogress的css库
 import 'nprogress/nprogress.css'
-
 // 引入全局状态管理 Pinia
-import { createPinia } from 'pinia'
+import pinia from "@/stores/index.js";
 
-const pinia = createPinia()
+
 
 // 创建应用，并将 App 根组件挂载到 <div id="#app"></div> 中
 const app = createApp(App)
