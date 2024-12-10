70cb43f6d03b922b0690456ca515b64087e56ceb
mirror <mirrorcloud3@gmail.com>
2024-12-10 16:32:56 +0800

    针对element plus组件默认是英文的修改
    Signed-off-by: mirror <mirrorcloud3@gmail.com>

--- vue_weblog/src/App.vue
+++ vue_weblog/src/App.vue
@@ -1,11 +1,16 @@
 <!-- html 代码 -->
 <template>
-<!--  <h1>Hello, Vue 3 !</h1>-->
-  <router-view></router-view>
+  <!-- 设置语言为中文 -->
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
+
 </template>
 
 <!--js 代码, setup 标识通常和组合式 API 搭配使用, 用于告诉 Vue 需要在编译时进行一些处理，让我们可以更简洁地使用组合式 API-->
 <script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const locale = zhCn
 </script>
 
 <!--css 代码， scoped 表示节点内 css 样式只针对此组件有效，不影响其他组件-->
